7d1dda52cdc806d62b56811f2e0adb80d7f1e1dd
Pablo Mochon <pimubiweb@gmail.com>
2021-11-11 08:38:41 +0100

    ejercicio1,2,6

---
+++ 4-Cookies/Relacion-1/ejercicio6/session.php
@@ -0,0 +1,18 @@
+<?php
+    /*
+    Aumentar y disminuir
+    Crear una sesión que aumente su valor en 1 o disminuya en 1.
+    Para ello se enviará un parámetro por GET , 
+    es decir en la URL de llamada al propio archivo,
+    y en función de si está a 1 o a 0 lo aumenta o lo disminuye el valor.
+    */
+
+    session_start();
+
+    if(!isset($_SESSION["numero"]))
+    
+
+
+
+    
+?>
