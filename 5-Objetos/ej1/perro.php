bac2484e76c910496bdc57c21af8c450a0a93bbc
Pablo Mochon <pimubiweb@gmail.com>
2021-11-16 14:36:00 +0100

    in progress

--- 5-Objetos/ej1/perro.php
+++ 5-Objetos/ej1/perro.php
@@ -1,4 +1,25 @@
 <?php
+    class NombreClase {
+        // Declaración de una propiedad
+        public $variable = 'Un valor cualquiera';
+        // Declaración de un método
+        public function mostrarVariable(){
+            echo $this->variable;
+        }
+    }
 
-    
+    class Perro {
+        
+        public string $tamanio;
+        
+        public function mostrar_propiedades(){
+            echo $this->tamanio;
+        }
+
+    }
+
+    $a = new Perro();
+    $a->tamanio = 'XL';
+    $var_dump(a);
+
 ?>
