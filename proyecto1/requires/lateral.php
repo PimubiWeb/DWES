cdcea11c9235c1c808cba702be608b027759c704
PabloPC <pimubiweb@gmail.com>
2021-12-11 21:10:58 +0100

    poco a poco

--- proyecto1/requires/lateral.php
+++ proyecto1/requires/lateral.php
@@ -1,3 +1,29 @@
-<div class="barra lateral">
-    
+<div id="sidebar">
+ 
+<div id="buscador" class="bloque"></div>
+    <h3>Buscar</h3>
+
+    <form action="buscar.php" method="POST">
+            <input type="text" name="busqueda">
+            <input type="submit" name="Buscar">
+    </form>
+
+    <?php if(isset($_SESSION['usuario']))?>
+        <div id="usuario-logueado" class="bloque">
+            <h3>Bienvenido, <?php $_SESSION['usuario']['nombre']?></h3>
+            <!--botones-->
+            <a href="crear-entradas.php" class="boton boton-verde">Crear entrada</a>
+            <a href="crear-categoria.php" class="boton">Crear categoria</a>
+            <a href="mis-datos" class="boton boton-naranja">Mis datos</a>
+            <a href="crear-categoria.php" class="boton">Cerrar sesión</a>
+        </div>
+        <?php if(isset($_SESSION['usuario']))?>
+        <div id="login" class="bloque">
+            <h3>Identificate</h3>
+            <?php if(isset($_SESSION['error_login']))?>
+                <div class="alerta alerta-error">
+                        //TODO iepaaa
+                </div>  
+
+        </div>
 </div>
